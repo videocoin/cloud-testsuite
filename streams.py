6d07f2ae88455c2224d7c5cf2d275fb65cd01e50
Dmitry Serkin <dmitry.serkin@gmail.com>
2019-10-18 17:08:37 -0700

    streams test extended

---
+++ streams.py
@@ -0,0 +1,68 @@
+from utils import pprint, ffmpeg_rtmp
+from api import API
+import argparse
+import logging
+import time
+import os
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s %(message)s')
+logger = logging
+
+def main():
+    parser = argparse.ArgumentParser(description='streams')
+    parser.add_argument('--env', default='studio.snb')
+    parser.add_argument('--username', default=os.environ.get('USERNAME', None))
+    parser.add_argument('--password', default=os.environ.get('PASSWORD', None))
+    parser.add_argument('--input', default='screen')
+
+    args = parser.parse_args()
+    if not args.username or not args.password:
+        exit(parser.print_usage())
+
+    api = API(args.env, verbose=False)
+
+    username = args.username
+    password = args.password
+
+    api.user('auth', {
+        'email': username,
+        'password': password,
+    })
+
+    pp = api.profiles('list')['items']
+
+    s = api.stream(
+        'create', {
+            'name': 'test01',
+            'profile_id': pp[0]['id']
+        })
+
+    s = api.stream(
+        'run', {
+            'id': s['id'],
+        })
+
+    while True:
+        time.sleep(3)
+        s = api.stream(
+            'get', {
+                'id': s['id'],
+            })
+
+        logger.debug(s)
+
+        if s['status'] in ['STREAM_STATUS_CANCELED', 'STREAM_STATUS_PREPARED', 'STREAM_STATUS_FALED']:
+            break
+
+    logger.info(s)
+
+    if s['status'] in ['STREAM_STATUS_CANCELED', 'STREAM_STATUS_FALED']:
+        logger.error('stream failed')
+        return
+
+    ffmpeg_rtmp(args.input, s['rtmp_url'])
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 
+import subprocess
 import random
 import string
 import json
@@ -8,3 +9,17 @@ def rand():
 
 def pprint(obj):
     print(json.dumps(obj, indent=4))
+
+def ffmpeg_rtmp(source, destination):
+    cmd = ['ffmpeg', '-re', '-i', source, '-c', 'copy', '-f', 'flv', destination]
+    if source == 'screen':
+        cmd = [
+            'ffmpeg',
+            '-f', 'lavfi', '-i', 'anullsrc=channel_layout=stereo:sample_rate=44100',
+            '-f', 'avfoundation', '-pix_fmt', 'uyvy422', '-i', 'Capture screen 0',
+            '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', 
+            '-preset', 'medium', '-r', '30', '-g', '30', '-b:v', '2500k',
+            '-acodec', 'libmp3lame', '-ar', '44100', '-threads', '6', '-b:a', '712000', '-bufsize', '512k',
+            '-f', 'flv', destination]
+    
+    subprocess.check_output(cmd)
